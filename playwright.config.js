d0a89d5b102720c6fe641b4e8622a42c84a1714f
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2025-05-26 15:34:36 -0700

    Add e2e test

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -77,5 +77,5 @@ export default defineConfig({
     url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
   },
-});
+})
 
