ebfadd0970647763f1882dc27d1327fb9091a5ee
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2025-05-26 17:04:54 -0700

    Test Render by creating bad health check

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...')
   res.send('ok')
 })
 
